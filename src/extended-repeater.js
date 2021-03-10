9ed2736784aabeab69edceda7494d372147039a4
Nickita Lashch <holbootla@gmail.com>
2021-03-10 22:32:18 +0300

    feat: finish carbon-dating, count-cats, dream-team, extendet-repeater, hanoi-tower, simple-chain, transform-array, what-season tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, {
+  repeatTimes = 1,
+  additionRepeatTimes = 1,
+  addition = '',
+  separator = '+',
+  additionSeparator = '|',
+  }) {
   
+  const addStr = Array(additionRepeatTimes)
+    .fill(String(addition))
+    .join(`${additionSeparator}`)
+
+  const mainStr = Array(repeatTimes)
+    .fill(str + addStr)
+    .join(`${separator}`)
+
+  return mainStr
+};
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error
+  }
+
+  const newArr = Array.from(arr)
+
+  for (let i = 0; i < newArr.length; i++)
+  
+    if (newArr[i] === '--discard-next') {
+      newArr[i] = undefined
+      if (i < (newArr.length - 1)) {
+        newArr[i + 1] = undefined
+      }
+    } else if (newArr[i] === '--discard-prev') {
+      newArr[i] = undefined
+      if (i != 0) {
+        newArr[i - 1] = undefined
+      }
+    } else if (newArr[i] === '--double-next') {
+      if (i < (newArr.length - 1)) {
+        newArr[i] = newArr[i + 1]
+      } else {
+        newArr[i] = undefined
+      }      
+    } else if (newArr[i] === '--double-prev') {
+      if  (i != 0) {
+        newArr[i] = newArr[i - 1]
+      } else {
+        newArr[i] = undefined
+      }
+    }
+
+  return newArr.filter(el => el !== undefined)
 };
