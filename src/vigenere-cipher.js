48bd5c871ab2ca9b345c602ed59ef9e124fad0f3
Nickita Lashch <holbootla@gmail.com>
2021-03-29 20:09:13 +0300

    feat: finish vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+
+  constructor(direction = true) {
+    this.direction = direction
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  }
+
   encrypt(message, key) {
     
     if (message === undefined || key === undefined) {
       throw new Error
     }
-
-    throw new CustomError('Not implemented');
+ 
+    const inputMessage = message.toUpperCase().split('')
+    const inputKey = key.toUpperCase().split('')
+    let outputMessage = []
+    let j = 0
+    
+    for (let i = 0; i < inputMessage.length; i++) {
+    
+      if (this.alphabet.indexOf(inputMessage[i]) !== -1) {      
+        const outputIndex = Math.abs(this.alphabet.indexOf(inputMessage[i]) + this.alphabet.indexOf(inputKey[j]) >= this.alphabet.length) ?
+                            Math.abs(this.alphabet.indexOf(inputMessage[i]) + this.alphabet.indexOf(inputKey[j]) - this.alphabet.length) :
+                            Math.abs(this.alphabet.indexOf(inputMessage[i]) + this.alphabet.indexOf(inputKey[j]))      
+        outputMessage.push(this.alphabet[outputIndex])                  
+      } else {      
+        outputMessage.push(inputMessage[i])
+        j -=1        
+      }
+      
+      j += 1
+      
+      if (j >= (inputKey.length)) {
+        j = 0
+      }
+      
+    }
+    
+    if (this.direction === false) {
+      return outputMessage.reverse().join('')
+    } else {
+      return outputMessage.join('')
+    } 
 
   } 
 
@@ -16,11 +51,40 @@ class VigenereCipheringMachine {
     if (message === undefined || key === undefined) {
       throw new Error
     }
-
-    throw new CustomError('Not implemented');
+ 
+    const inputMessage = message.toUpperCase().split('')
+    const inputKey = key.toUpperCase().split('')
+    let outputMessage = []
+    let j = 0
+    
+    for (let i = 0; i < inputMessage.length; i++) {
+    
+      if (this.alphabet.indexOf(inputMessage[i]) !== -1) {      
+        const outputIndex = Math.abs(this.alphabet.indexOf(inputMessage[i]) - this.alphabet.indexOf(inputKey[j]) < 0) ?
+                            Math.abs(this.alphabet.indexOf(inputMessage[i]) - this.alphabet.indexOf(inputKey[j]) + this.alphabet.length) :
+                            Math.abs(this.alphabet.indexOf(inputMessage[i]) - this.alphabet.indexOf(inputKey[j]))      
+        outputMessage.push(this.alphabet[outputIndex])                  
+      } else {      
+        outputMessage.push(inputMessage[i])
+        j -=1        
+      }
+      
+      j += 1
+      
+      if (j >= (inputKey.length)) {
+        j = 0
+      }
+      
+    }
+    
+    if (this.direction === false) {
+      return outputMessage.reverse().join('')
+    } else {
+      return outputMessage.join('')
+    }
     
   }
 
 }
 
-module.exports = VigenereCipheringMachine;
+module.exports = VigenereCipheringMachine
